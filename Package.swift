c90205cb68426af3b7ee27ccd69656a864a654d5
gi8 <giottolino@gmail.com>
2026-08-11 00:07:33 +0200

    feat: bump easybar-kit

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/easybar-app/easybar-kit",
-      from: "0.1.0"
+      from: "0.2.0"
     )
   ],
   targets: [
